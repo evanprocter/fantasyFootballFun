33a586c708b0311958098fe2abb04a6ff1aaea71
Evan Procter <evanprocter@gmail.com>
2018-11-07 10:56:57 -0500

    added teams to team table

---
+++ schema.sql
@@ -0,0 +1,28 @@
+-- USERs
+-- name
+create table teams (
+    id serial primary key,
+    name text
+)
+
+
+create table players (
+    id serial primary key,
+    number integer,
+    position text,
+    team_id integer references teams (id),
+    name text
+)
+
+-- quartback/s
+-- id
+-- name
+-- passing and running statistics
+-- create table qbStats (
+--     id serial primary key,
+--     name text,
+--     passingYDs integer,
+--     passingINTs integer,
+--     rushing integer,
+
+-- )
